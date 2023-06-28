42df0c879441bb2625f32c495f0d781b12ba39f2
doma2k <79820904+doma2k@users.noreply.github.com>
2023-06-28 19:18:07 +0100

    fair: GET/users add

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import http from 'http';
+import fs from 'fs';
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
-    res.end();
-  })
-  .listen(8080);
+const server = http.createServer(function (req, res) {
+  const method = req.method;
+  switch (method) {
+    case 'GET':
+      if (req.url === '/users') {
+        fs.readFile('./db.json', 'utf-8', (error, data) => {
+          if (error) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error reading file');
+          } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(data);
+          }
+        });
+      }
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Route not found');
+  }
+});
+
+server.listen(8080, () => console.log('Server up, and running!'));
